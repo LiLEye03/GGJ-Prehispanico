3ff213180c0e12a3cee7706c8b8782c1c341a141
CharloHdz <84859398+CharloHdz@users.noreply.github.com>
2023-01-31 10:30:36 -0600

    Personaje, Mapa y Colliders
    Actualizaciones:
    - Personaje principal con movimiento Top Down
    - Colliders a niveles
    - Mapa en Unity

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
     public static string gameStat;
+    public static string miniGameStat;
+    [SerializeField] GameObject miniGamePanel, godsPanel, deathPanel, cacaoPanel;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,29 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch(miniGameStat){
+
+            case "GODS":
+                miniGamePanel.SetActive(true);
+                godsPanel.SetActive(true);
+                cacaoPanel.SetActive(false);
+                deathPanel.SetActive(false);
+                break;
+            
+            case "CACAO":
+                miniGamePanel.SetActive(true);
+                godsPanel.SetActive(false);
+                cacaoPanel.SetActive(true);
+                deathPanel.SetActive(false);
+                break;
+                
+            case "DEATH":
+                miniGamePanel.SetActive(true);
+                godsPanel.SetActive(false);
+                cacaoPanel.SetActive(false);
+                deathPanel.SetActive(true);
+                break;
+        }
+
     }
 }
---
+++ Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontal, vertical);
+        rb.velocity = movement * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("GODS")){
+            GameManager.miniGameStat = "GODS";
+        }
+        else if (other.gameObject.CompareTag("DEATH")){
+            GameManager.miniGameStat = "DEATH";
+        }
+        else {
+            GameManager.miniGameStat = "CACAO";
+        }
+    }
+}
